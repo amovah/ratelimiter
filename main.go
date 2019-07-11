39a94c495fdbcb5db96b5a869a0d81fd6b8f935c
Ali Movahedi <ali_movahedi@aol.com>
2019-07-11 23:30:24 +0430

    style: lint

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-  "encoding/json"
-  "net/http"
-  "strings"
-  "io/ioutil"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"time"
 )
 
@@ -19,69 +19,47 @@ func ProxyResponse(origin http.Response, target http.ResponseWriter) {
 	body, err := ioutil.ReadAll(origin.Body)
 	if err != nil {
 		fmt.Fprint(target, "ERROR")
-	} else {
-		for key, value := range origin.Header {
-			target.Header().Set(key, strings.Join(value, ", "))
-		}
-
-		target.Write(body)
+		return
 	}
+
+	for key, value := range origin.Header {
+		target.Header().Set(key, strings.Join(value, ", "))
+	}
+
+	target.Write(body)
 }
 
 func proxy(res http.ResponseWriter, req *http.Request) {
-	if record[req.RemoteAddr] <= uint64(config.RequestPerDuration) {
-		record[req.RemoteAddr] = record[req.RemoteAddr] + 1
-
-		go func() {
-			time.Sleep(time.Duration(config.Duration) * time.Millisecond)
-			record[req.RemoteAddr] = record[req.RemoteAddr] - 1
-		}()
-
-		req.URL.Path = config.TargetServer + req.URL.Path
-
-		if req.Method == "GET" {
-	    response, err := http.Get(req.URL.Path)
-
-	    if err != nil {
-				res.WriteHeader(400)
-				fmt.Fprint(res, err)
-				return
-	    }
-
-			ProxyResponse(*response, res)
-	  }
-
-		if req.Method == "POST" {
-			response, err := http.Post(req.URL.Path, req.Header.Get("Content-Type"), req.Body)
-
-	    if err != nil {
-				res.WriteHeader(400)
-				fmt.Fprint(res, err)
-				return
-	    }
-
-			ProxyResponse(*response, res)
-		} else {
-			createdReq, err := http.NewRequest(req.Method, req.URL.Path, req.Body)
-			if err != nil {
-				res.WriteHeader(400)
-				fmt.Fprint(res, err)
-				return
-			}
-
-			client := http.Client{}
-			response, err := client.Do(createdReq)
-			if err != nil {
-				res.WriteHeader(400)
-				fmt.Fprint(res, err)
-				return
-			}
-
-			ProxyResponse(*response, res)
-		}
-	} else {
+	if record[req.RemoteAddr] > uint64(config.RequestPerDuration) {
 		http.Error(res, "You reach your limit", 429)
+		return
 	}
+
+	record[req.RemoteAddr] = record[req.RemoteAddr] + 1
+
+	go func() {
+		time.Sleep(time.Duration(config.Duration) * time.Millisecond)
+		record[req.RemoteAddr] = record[req.RemoteAddr] - 1
+	}()
+
+	req.URL.Path = config.TargetServer + req.URL.Path
+
+	createdReq, err := http.NewRequest(req.Method, req.URL.Path, req.Body)
+	if err != nil {
+		res.WriteHeader(400)
+		fmt.Fprint(res, err)
+		return
+	}
+
+	client := http.Client{}
+	response, err := client.Do(createdReq)
+	if err != nil {
+		res.WriteHeader(400)
+		fmt.Fprint(res, err)
+		return
+	}
+
+	ProxyResponse(*response, res)
 }
 
 var config Config
@@ -98,7 +76,7 @@ func main() {
 
 	record = make(map[string]uint64)
 
-  http.HandleFunc("/", proxy)
+	http.HandleFunc("/", proxy)
 
-  http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
